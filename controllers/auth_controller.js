1b5348250802c9f5c0781e6f88de0f81ae2186c2
nyros <nageswararao.nyros@gmail.com>
2021-01-12 11:37:04 +0530

    addded

---
+++ controllers/auth_controller.js
@@ -0,0 +1,53 @@
+const users = require('../models/user');
+const bcrypt = require('bcrypt')
+
+
+exports.login = (req, res)=>{
+	console.log(req.body)
+	users.findOne({email: req.body.email}, function(err, user){
+		if(user && user.email !== ''){
+			if(!user.verified){
+				res.json({
+						status: 400,
+						message: "Please Verify Email Address"
+					})
+			}else{
+				bcrypt.compare(req.body.password, user.password, (err, isValid) => {
+					console.log(isValid)
+					if(isValid){
+						res.json({
+							status: 200,
+							data: user
+						})
+					}else{
+						res.json({
+							status: 400,
+							message: "Incorrect password"
+						})
+					}
+					
+				})
+			}
+		}else{
+			res.json({
+				status: 404,
+				message: "User Not Found"
+			})
+		}
+	})
+}
+
+exports.get_data = (req, res) =>{
+
+
+	console.log(req.params)
+
+	res.json({
+		data: "Data fetched by nageswar",
+		status: 200
+	})
+}
+
+
+
+
---
+++ routes/api_routes.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+var usersController = require('../controllers/user_controller')
+var dashboardController = require('../controllers/dashboard_controller')
+var authController = require('../controllers/auth_controller')
+
+router.route('/login').post(authController.login)
+router.route('/signUp').post(usersController.save_user)
+
+router.route('/getNodeJsTrainingData/:name').get(authController.get_data);
+
+
+module.exports = router;
